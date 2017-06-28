7852e4a61475b10d1cb878ae5ce90460f60452bc
Keeley Hammond <keeleymhammond@gmail.com>
2017-06-28 10:55:56 -0700

    Changed blog template

--- blog/html.js
+++ blog/html.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import Helmet from "react-helmet"
+import DocumentTitle from 'react-document-title'
 import { prefixLink } from 'gatsby-helpers'
-import { GoogleFont, TypographyStyle } from 'react-typography'
-import typography from './utils/typography'
 
 const BUILD_TIME = new Date().getTime()
 
@@ -11,9 +9,12 @@ module.exports = React.createClass({
   propTypes: {
     body: React.PropTypes.string,
   },
-  render () {
-    const { body } = this.props
-    const head = Helmet.rewind();
+  render() {
+    const { body, route } = this.props
+    const title = DocumentTitle.rewind()
+    const jquery = <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" ></script>
+    const tether = <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" ></script>
+    const bootstrap = <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" ></script>
 
     let css
     if (process.env.NODE_ENV === 'production') {
@@ -25,18 +26,17 @@ module.exports = React.createClass({
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          {head.title.toComponent()}
-          {head.meta.toComponent()}
-          <TypographyStyle typography={typography} />
-          <GoogleFont typography={typography} />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
+          <title>
+            {title}
+          </title>
           {css}
+          {jquery}
+          {tether}
+          {bootstrap}
         </head>
         <body>
-          <div id="react-mount" dangerouslySetInnerHTML={{ __html: body }} />
+          <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
         </body>
       </html>
